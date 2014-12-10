1411159c22c41c279e9c4995f8f18e20312213ba
Anirud <anirudt@gmail.com>
2014-12-10 22:49:18 +0530

    Added first file for scikit learn

---
+++ sub.py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn import grid_search
+from sklearn import cross_validation as cv
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+from sklearn.cross_validation import StratifiedKFold
+
+workDir = r'C:\Transit\Kaggle\\'
+
+# Read data
+train = np.genfromtxt(open(workDir + 'train.csv','rb'), delimiter=',')
+target = np.genfromtxt(open(workDir + 'trainLabels.csv','rb'), delimiter=',')
+test = np.genfromtxt(open(workDir + 'test.csv','rb'), delimiter=',')
+
+# This takes in the input without compromising the first row
+
+pca = PCA(n_components=6,whiten=True)
+train = pca.fit_transform(train)
+test = pca.transform(test)
+
+'''
+Basically, a PCA is done to reduce higher dimensional problem to a
+lower dimensional problem.
+'''
+print "hello"
+
+'''
+preparing the parameters
+'''
+C_range = 10.0 ** np.arange(7,10)
+gamma_range = 10.0 ** np.arange(-4,0)
+
+print "hello"
+params = dict(gamma=gamma_range,C=C_range)
+cvk = StratifiedKFold(target,3)
+print "hello"
+classifier = SVC()
+print "hello"
+clf = grid_search.GridSearchCV(classifier,param_grid=params,cv=cvk) # SVM Classifier buildup.
+print "hello"
+clf.fit(train,target)
+#print("The best classifier is: ",clf.best_estimator_) 
+
+scores = cv.cross_val_score(clf.best_estimator_, train, target, cv=30)
+print('Estimated score: %0.5f (+/- %0.5f)' % (scores.mean(), scores.std() / 2))
+
+#answer = 0.92784 for n_components = 12 [initial value]
